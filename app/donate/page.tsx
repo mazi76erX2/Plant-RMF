37d3a231812204a6a06d52bdfee3f8ae0a0a9b19
Xolani <mazi76erx@gmail.com>
2025-05-04 12:00:32 +0000

    Remove unused Link import from donate page

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { motion } from "framer-motion";
 
 export default function DonatePage() {
